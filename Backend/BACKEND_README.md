e05cf9a9a635be8ae164aeec51f3adf901a35e3c
Kaifmanzar26MAR <kaifmanzar321@gmail.com>
2025-01-01 23:05:20 +0530

    implement user login feature with validation and error handling

--- Backend/BACKEND_README.md
+++ Backend/BACKEND_README.md
@@ -19,6 +19,7 @@
 ```
 
 **Response:**
+
 **201 Created**
 **Description** user registered successfully.
 **Body**
@@ -38,11 +39,80 @@
 ```
 
 **400 Bad Request**
+**Description** User already exists.
+**Body**
+```json
+{
+  "message": "User already exists"
+}
+```
+**401 Bad Request**
+**Description** All fields are required or Invalid filed lenght.
+**Body**
+```json
+{
+  "message": "All fields are required or Invalid filed lenght"
+}
+```
+
+**500 Internal Server Error**
+**Description** An error occurred on the server.
+**Body**
+```json
+{
+  "message": "Error message"
+}
+```
+
+
+### Login User
+
+**Endpoint:** `POST /api/v1/users/login`
+
+**Description:** This endpoint is used to login a user.
+
+**Request Body:**
+```json
+{
+  "email": "string (required, unique, minimum 6 characters)",
+  "password": "string (required)"
+}
+```
+
+**Response:**
+**201 logined**
+**Description** user logined successfully.
+**Body**
+```json
+{
+  "message": "User logged in successfully",
+  "user": {
+    "_id": "string",
+    "fullname": {
+      "firstname": "string",
+      "lastname": "string"
+    },
+    "email": "string"
+  },
+  "token": "string"
+}
+```
+
+**400 Bad Request**
 **Description** All fields are required.
 **Body**
 ```json
 {
   "message": "All fields are required"
+}
+```
+
+**401 Unauthorized**
+**Description** Invalid email or password
+**Body**
+```json
+{
+  "message": "Invalid email or password"
 }
 ```
 
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,12 @@
 const userModel = require("../models/user.model.js");
 const userService = require("../services/user.service.js");
 
+const validateEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email);
+}
+
+
 //*Register a user
 const registerUser = async (req, res) => {
     try {
@@ -13,7 +19,30 @@ const registerUser = async (req, res) => {
        //*Add validation for the required data
         const { firstname, lastname, email, password } = req.body;
         if (!firstname || !email || !password || firstname.trim() === "" || email.trim() === "" || password.trim() === "") {
-            throw new Error("All fields are required");
+            return res.status(401).json({ message: "All fields are required!" });
+        }
+
+        if(firstname.length < 3) {
+            return res.status(401).json({ message: "Firstname must be at least 3 characters long!" });
+        }
+        if(lastname.length < 3) {
+            return res.status(401).json({ message: "Lastname must be at least 3 characters long!" });
+        }
+        if(email.length < 6) { 
+            return res.status(401).json({ message: "Email must be at least 6 characters long!" });
+        }
+        if(password.length < 6) {
+            return res.status(401).json({ message: "Password must be at least 6 characters long!" });
+        }
+
+        if(!validateEmail(email)) {
+            return res.status(401).json({ message: "Invalid email address!" });
+        }
+
+        //*check if user already exists
+        const userExists = await userModel.find({ email });
+        if(userExists.length > 0) {
+            return res.status(400).json({ message: "User already exists!" });
         }
 
         //*Create a user
@@ -41,4 +70,55 @@ const registerUser = async (req, res) => {
     }
 }
 
-module.exports = { registerUser };
+
+const loginUser = async (req, res) => {
+    try {
+        //*Check if user details are provided
+        if(!req.body) {
+            throw new Error("Please provide user details");
+        }
+
+        //*Add validation for the required data
+        const { email, password } = req.body;
+        if (!email || !password || email.trim() === "" || password.trim() === "") {
+            throw new Error("All fields are required");
+        }
+
+        if(email.length < 6) { 
+            return res.status(401).json({ message: "Email must be at least 6 characters long!" });
+        }
+        if(password.length < 6) {
+            return res.status(401).json({ message: "Password must be at least 6 characters long!" });
+        }
+
+        if(!validateEmail(email)) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        //*Find user by email
+        const user = await userModel.findOne({ email }).select("+password");
+
+        //*Check if user exists
+        if(!user) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        //*Check if password is correct
+        const isMatch = await user.comparePassword(password);
+        if(!isMatch) {
+            return res.status(401).json({ message: "Invalid email or password" });
+        }
+
+        //*Generate token
+        const token = user.generateAuthToken();
+
+        //*send response
+        res.status(200).json({ message: "User logged in successfully", user, token });
+
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+
+
+module.exports = { registerUser, loginUser };
