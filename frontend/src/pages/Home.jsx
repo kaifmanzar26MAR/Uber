04a95c5e24abb907c329837af4341962f3c98163
Kaifmanzar26MAR <kaifmanzar321@gmail.com>
2025-01-07 10:03:41 +0530

    add captain signup page and update routing; enhance user login page with form and navigation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='h-screen w-full bg-cover bg-center bg-[url(./assets/home/traffic-light-bg-1.jpg)] flex flex-col justify-between items-start pt-5'>
+      <img src="./assets/home/uber-logo.png" alt="UBER" className="w-20 ml-8 bg-slate-50 rounded px-2" />
+      <div className='bg-white pb-7 py-4 px-4 w-full'>
+        <h2 className='text-2xl font-bold'>Get Started with Uber</h2>
+        <Link to={"/login"} className='flex items-center justify-center w-full bg-black text-white py-3 rounded mt-5'>Continue</Link>
+      </div>
+    </div>
   )
 }
 
--- frontend/src/pages/UserLogin.jsx
+++ frontend/src/pages/UserLogin.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const UserLogin = () => {
   return (
-    <div>UserLogin</div>
-  )
-}
+    <div className="p-7 h-screen justify-between flex flex-col">
+      <div>
+        <img src="./assets/home/uber-logo.png" alt="UBER" className="w-16 mb-4" />
+        <from>
+          <h3 className="text-lg mb-2 font-semibold">What's your email</h3>
+          <input 
+            type="email" 
+            className="bg-[#eeeeee] focus-within:outline-black rounded px-3 py-3 mb-7 border w-full text-lg placeholder:text-base"
+            required 
+            placeholder="email@example.com"
+          />
+          <h3 className="text-lg mb-2 font-semibold">Enter Password</h3>
+          <input 
+            type="password" 
+            className="bg-[#eeeeee] focus-within:outline-black rounded px-3 py-3 mb-7 border w-full text-lg placeholder:text-base"
+            required 
+            placeholder="password"
+          />
+          <button 
+            className="bg-[#111] text-white font-semibold rounded px-3 py-3 mb-3  border w-full text-lg placeholder:text-base"
+          >Login</button>
+        </from>
+        <p className="text-center">New Here? <Link to={'/signup'}  className=" text-blue-600">Create New Account</Link></p>
+      </div>
+      <div>
+        <button
+            className="bg-[#10b461] text-white font-semibold rounded px-3 py-3 mb-7  border w-full text-lg placeholder:text-base"
+        >Sign in as Captain</button>
+      </div>
+      
+    </div>
+  );
+};
 
-export default UserLogin
+export default UserLogin;
